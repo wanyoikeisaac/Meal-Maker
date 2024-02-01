cb9da801bde6f527c0f5bcebc98526dc5b0d1d9e
wanyoikeisaac <wanyoikemuiruisaac@gmail.com>
2024-02-01 17:06:36 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const menu = {
+    _menu: '',
+    _price: 0,
+    
+  
+      
+    set meal(mealToCheck){
+      if(typeof mealToCheck === 'string'){
+        return this._meal = mealToCheck;
+      }
+      },
+  
+      set price(priceToCheck){
+        if(typeof priceToCheck === 'number'){
+          return this._price = priceToCheck;
+        }
+      },
+      get todaysSpecial(){
+        if(this._meal && this._price){
+          return `Today's Meal is ${this._meal} for $${this._price}!`
+        } else {
+          return `Meal or price not set correctly!`
+        }
+      }
+      
+      
+    };
+  menu.meal = 'Pizza';
+  menu.price = 0;
+  console.log(menu.todaysSpecial);
+  
